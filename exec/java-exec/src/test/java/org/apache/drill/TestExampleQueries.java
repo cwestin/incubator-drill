8636e6d96a535c02b461659d3a3aacd527c34692
Chris Westin <cwestin@yahoo.com>
2015-01-08 13:57:20 -0800

    TEMP: extended test cases

--- exec/java-exec/src/test/java/org/apache/drill/TestExampleQueries.java
+++ exec/java-exec/src/test/java/org/apache/drill/TestExampleQueries.java
@@ -414,6 +414,26 @@ public class TestExampleQueries extends BaseTestQuery{
     test("select case when n_nationkey > 0 and n_nationkey < 2 then concat(n_name, '_abc') when n_nationkey >=2 and n_nationkey < 4 then '_EFG' else concat(n_name,'_XYZ') end from cp.`tpch/nation.parquet` ;");
   }
 
+  @Test
+  public void testCase1() throws Exception {
+    test("select concat(n_name,'_XYZ') from cp.`tpch/nation.parquet` ;");
+  }
+
+  @Test
+  public void testCase2() throws Exception {
+    test("select case when n_nationkey > 0 and n_nationkey < 2 then concat(n_name, '_abc') else concat(n_name,'_XYZ') end from cp.`tpch/nation.parquet` ;");
+  }
+
+  @Test
+  public void testCase3() throws Exception {
+    test("select case when n_nationkey > 0 and n_nationkey < 2 then concat(n_name, '_abc') when n_nationkey >=2 and n_nationkey < 4 then concat(n_name, '_EFG') else concat(n_name,'_XYZ') end from cp.`tpch/nation.parquet` ;");
+  }
+
+  @Test
+  public void testCase4() throws Exception {
+    test("select case when n_nationkey > 0 and n_nationkey < 2 then concat(n_name, '_abc') else '_XYZ' end from cp.`tpch/nation.parquet` ;");
+  }
+
   @Test // tests join condition that has different input types
   public void testJoinCondWithDifferentTypes() throws Exception {
     test("select t1.department_description from cp.`department.json` t1, cp.`employee.json` t2 where (cast(t1.department_id as double)) = t2.department_id");
